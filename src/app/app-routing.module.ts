63c48ed8268b51d379b778894c0c566d2c2d89bb
SanjanaMuralidhar22 <143344975+SanjanaMuralidhar22@users.noreply.github.com>
2023-10-04 09:47:16 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'index.component',pathMatch:'full'},
+  // {path:'about',component:AboutComponent},
+  // {path:'contact',component:ContactComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { AboutComponent } from './Components/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    ContactComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
